edf9f2ddd23e80fe21c87704d8015f670cf4d8a3
Vembarasan Nagarajan <67158263+vembarasugn@users.noreply.github.com>
2023-07-29 03:10:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/github-pages',
+
+}
 
 module.exports = nextConfig
