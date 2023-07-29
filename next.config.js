d7435d2db5600f5c43a305fba507ca8f17587db0
Vembarasan Nagarajan <vembarasugn@gmil.com>
2023-07-29 22:01:57 +0530

    metadata from a component marked with use client, which is disallowed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  reactStrictMode: true,
-
- // Add basePath
-  basePath: '/github-pages',
-
-}
-
-module.exports = nextConfig
-=======
     reactStrictMode: true,
   
    // Add basePath
@@ -18,4 +8,3 @@ module.exports = nextConfig
   }
   
   module.exports = nextConfig;
->>>>>>> 76e85f6 (Github pages commit)
